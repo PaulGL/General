1fc89ef68df875d2cbe259ffd126ef0272d97ae2
Empon <Empon@pc10.home>
2015-01-25 15:23:38 +0100

    Initial Commit

---
+++ TransitionZoom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitionZoom
+//
+//  Created by Empon on 25/01/2015.
+//  Copyright (c) 2015 Empon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransitionZoom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitionZoom
+//
+//  Created by Empon on 25/01/2015.
+//  Copyright (c) 2015 Empon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
